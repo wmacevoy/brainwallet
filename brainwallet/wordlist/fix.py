30de8d865915b364d2bfb7400301c174e328f6d0
Warren MacEvoy <wmacevoy@gmail.com>
2019-03-14 12:22:59 -0600

    french is changed (194/2048)=0.094727

--- brainwallet/wordlist/fix.py
+++ brainwallet/wordlist/fix.py
@@ -10,6 +10,7 @@ sys.path.insert(0,parentdir)
 from phrases import Phrases
 
 languages = [
+    "ab",
     "chinese_simplified", 
     "chinese_traditional", 
     "english", 
@@ -27,32 +28,68 @@ for el1 in range(len(languages)):
     words = phrases.words[:]
     sequence = [None for i in range(len(words))]
     reuses = False
+    changed = False
+
+    # put already reserved words at their index
     for i in range(len(words)):
         word=words[i]
+        if word == None: continue
         if word in reserved:
             words[i]=None
+            if phrases.invWords[word] != reserved[word]:
+                changed=True
             sequence[reserved[word]]=word
             reuses = True
+
+    # put new words in their place if not already reserved:
+    for i in range(len(words)):
+        word=words[i]
+        if word == None: continue
+        if words[i] in reserved: continue
+        if sequence[i] != None: continue
+        words[i] = None
+        sequence[i] = word
+
+    # put remaining words in sequence
     k=0
-    changed = False
     for i in range(len(words)):
         word=words[i]
         if word == None: continue
         while sequence[k] != None: k = k + 1
         sequence[k] = word
-        changed = changed or phrases.invWords[word] != k
         k = k + 1
 
-    for k in range(len(words)):
-        reserved[sequence[k]] = k
+    assert len(sequence)==len(words)
+    for k in range(len(sequence)):
+        if not sequence[k] in reserved:
+            reserved[sequence[k]] = k
+        else:
+            assert reserved[sequence[k]] == k
+
+    changes = 0
+    for k in range(len(sequence)):
+        word=sequence[k]
+        oldIndex=phrases.invWords[word]
+        newIndex=k
+        if oldIndex != newIndex:
+            changes = changes + 1
 
     if reuses:
         print ("%s reuses words in previous dictionaries." % language)
     if not changed:
         print ("%s is unchanged." % language)
     else:
-        print ("%s is changed to:" % language)
+        print ("%s is changed (%d/%d)=%f:" % (language,changes,len(words),float(changes)/float(len(words))))
+        changes=0
+        for k in range(len(sequence)):
+            word=sequence[k]
+            oldIndex=phrases.invWords[word]
+            newIndex=k
+            if oldIndex != newIndex:
+                changes = changes + 1
+                print "Change #%d: %s moved from %d to %d" % (changes,word.encode('utf-8'),oldIndex,newIndex)
+        print ("%s new sequence:" % language)
         for word in sequence:
             print (word.encode('utf-8'))
-        print ("end of %s changes." % language)
+        print ("end of %s new sequence." % language)
         print ("")
