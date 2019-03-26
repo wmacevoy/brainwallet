5c6ca00d8483e714693724c5435e228934c39018
Warren MacEvoy <wmacevoy@gmail.com>
2019-03-26 02:13:28 -0600

    gf

---
+++ gf/irreducible.cpp
@@ -0,0 +1,44 @@
+// https://www.shoup.net/ntl/doc/tour-ex5.html
+
+#include <iostream>
+#include <NTL/GF2XFactoring.h>
+
+using namespace std;
+using namespace NTL;
+
+int main()
+{
+  const long maxBits = 1024;
+  std::cout << "SPARSE_IRREDUCIBLE_GF2X = [" << std::endl;
+  std::cout << "    ()," << std::endl;
+  std::cout << "    ()," << std::endl;
+  for (long bits = 2; bits <= maxBits; ++bits) {
+    GF2X poly;
+    BuildSparseIrred(poly,bits);
+    long d = deg(poly);
+
+    std::cout << "    (" << d;
+    for (long k = d-1; k >= 0; --k) {
+      GF2 c = coeff(poly,k);
+      if (IsOne(c)) { std::cout << "," << k; }
+    }
+
+    std::cout << ")" << (bits < maxBits ? "," : "") << " # ";
+
+    std::cout << "x^" << d;
+    for (long k = d-1; k >= 0; --k) {
+      GF2 c = coeff(poly,k);
+      if (IsOne(c)) {
+	if (k > 1) {
+	  cout << "+x^" << k;
+	} else if (k == 1) {
+	  cout << "+x";
+	} else if (k == 0) {
+	  cout << "+1";
+	}
+      }
+    }
+    std::cout << std::endl;
+  }
+  std::cout << "]" << std::endl;
+}
