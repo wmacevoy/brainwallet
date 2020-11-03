cb30bd903d808c76e1553f1ed425cdbcd31006a4
Warren MacEvoy <wmacevoy@gmail.com>
2020-11-02 17:55:03 -0700

    combination module

--- brainwallet/combinations.py
+++ brainwallet/combinations.py
@@ -23,49 +23,26 @@ class Combinations:
             t = (t*a)//b
             a -= 1
             b += 1
-            
-        print("choose(" + str(n) + "," + str(r) + ")=" + str(t))
         return t
 
     @classmethod
-    def _largestV(cls,a,b,x):
-        v = a - 1;
-        print("largestV(" + str(a) + "," + str(b) + "," + str(x) + ")")        
-        while (cls.choose(v, b) > x):
-            v -= 1
-        print("largestV(" + str(a) + "," + str(b) + "," + str(x) + ")=" + str(v))
-        return v
-
-    @classmethod
-    def _combinadic(cls,n,r,k):
-        """
-        Finds the combinadic of k for nCr.
-        
-        Combinadic of k corresponds to the array of [c_1, c_2, ..., c_r] such
-        that k = choose(c_1, r) + choose(c_2, r-1) + ... + choose(c_r, 1).
-        """
-        x = k
-        a = n
-        b = r
-        c = [0]*r
-        print("combinadic(" + str(n) + "," + str(r) + "," + str(k) + ")")
-        for i in range(r):
-            c[i] = cls._largestV(a, b, x)
-            x -= cls.choose(c[i], b)
-            a = c[i]
-            b -= 1
-        print("combinadic(" + str(n) + "," + str(r) + "," + str(k) + ")=" + str(c))            
-        return c
-
-    @classmethod
     def unrank(cls,n,r,k):
-        nCr = cls.choose(n, r)
-        if (nCr == 0):
-            return
-        d = nCr - k - 1
-        c=cls._combinadic(n, r, d)
+        j=k
+        c=[0]*r
         for i in range(r):
-            c[i] = n - c[i] - 1
+            if i > 0:
+                hi=c[i-1]
+            else:
+                hi=n
+            lo=r-i-1
+            while hi-lo > 1:
+                mid=(lo+hi)//2
+                if cls.choose(mid,r-i) <= j:
+                    lo=mid
+                else:
+                    hi=mid
+            c[i]=lo
+            j -= cls.choose(c[i],r-i)
         return c
     
     @classmethod    
@@ -78,10 +55,7 @@ class Combinations:
         return k
 
 def main():
-    
-    rng = RNG()
-    for i in range(1,len(sys.argv)):
-        print(rng.next(int(sys.argv[i])))
+    print("no main")
 
 if __name__ == "__main__":
     main()
--- brainwallettest/combinationstest.py
+++ brainwallettest/combinationstest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import inspect,math,os,sys,unittest
+import inspect,math,os,sys,unittest,time
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -10,27 +10,87 @@ sys.path.insert(0,parentdir+"/brainwallet")
 from combinations import Combinations
 
 class CombinationsTest(unittest.TestCase):
-    def _testCombinations(self,n,r,c):
+
+    def _testChoose(self,n,r,c):
         nCr=Combinations.choose(n,r)
         self.assertEqual(nCr,len(c))
+
+    def _testUnrank(self,n,r,c):
+        nCr=Combinations.choose(n,r)
         for k in range(nCr):
-            d = Combinations.get(n,r,k)
-            j = Combinations.index(n,r,c[k])
+            d = Combinations.unrank(n,r,k)
+        for k in range(nCr):
+            d = Combinations.unrank(n,r,k)
+            self.assertEqual(d,c[k])
+
+    def _testRank(self,n,r,c):
+        nCr=Combinations.choose(n,r)        
+        for k in range(nCr):
+            j = Combinations.rank(n,r,c[k])
+        for k in range(nCr):
+            j = Combinations.rank(n,r,c[k])
             self.assertEqual(k,j)
-            self.assertEqual(len(d),len(c[k]))
-            for i in range(r):
-                self.assertEqual(d[i],c[k][i])
-    def test5C2(self):
+
+    def get5C2(self):
+        return [[1, 0], [2, 0], [2, 1], [3, 0], [3, 1], [3, 2], [4, 0], [4, 1], [4, 2], [4, 3]]
+
+    def get5C3(self):
+        return [[2, 1, 0],[3, 1, 0],[3, 2, 0],[3, 2, 1],[4, 1, 0],[4, 2, 0],[4, 2, 1],[4, 3, 0],[4, 3, 1],[4, 3, 2]]
+
+    def test5C2Choose(self):
         n=5
         r=2
-        c=[[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]        
-        self._testCombinations(n,r,c)
+        c=self.get5C2()
+        self._testChoose(n,r,c)
 
-    def test5C3(self):
+    def test5C2Rank(self):
+        n=5
+        r=2
+        c=self.get5C2()
+        self._testRank(n,r,c)
+
+    def test5C2Unrank(self):
+        n=5
+        r=2
+        c=self.get5C2()
+        self._testUnrank(n,r,c)
+        
+    def test5C3Choose(self):
         n=5
         r=3
-        c=[[0,1,2],[0,1,3],[0,1,4],[0,2,3],[0,2,4],[0,3,4],[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
-        self._testCombinations(n,r,c)
+        c=self.get5C3()
+        self._testChoose(n,r,c)
 
+    def test5C3Rank(self):
+        n=5
+        r=3
+        c=self.get5C3()
+        self._testRank(n,r,c)
+
+    def test5C3Unrank(self):
+        n=5
+        r=3
+        c=self.get5C3()
+        self._testUnrank(n,r,c)
+
+    def testScale(self):
+        n=2048
+        r=1024
+        t0=time.time()
+        k=Combinations.choose(n,r)//2
+        t1=time.time()
+        tChoose=t1-t0
+        c=Combinations.unrank(n,r,k)
+        t2=time.time()
+        tUnrank=t2-t1
+        j=Combinations.rank(n,r,c)
+        t3=time.time()
+        tRank=t3-t2
+        print('choose time=%.3f, unrank time=%.3f, rank time=%.3f' % (tChoose,tUnrank,tRank))
+        self.assertLess(tChoose,0.01)
+        self.assertLess(tUnrank,0.1)
+        self.assertLess(tRank,0.01)
+        
+        
 if __name__ == '__main__':
     unittest.main()
